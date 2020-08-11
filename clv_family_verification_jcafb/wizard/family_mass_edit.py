34b7bbd8ea5d6ecc80a3bcb3f2eabf9f4a3aff23
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-08-11 14:37:30 -0300

    [IMP] clv_family_verification_jcafb (IMP wizard/family_mass_edit)

--- clv_family_verification_jcafb/wizard/family_mass_edit.py
+++ clv_family_verification_jcafb/wizard/family_mass_edit.py
@@ -12,6 +12,29 @@ _logger = logging.getLogger(__name__)
 class FamilyMassEdit(models.TransientModel):
     _inherit = 'clv.family.mass_edit'
 
+    ref_address_is_unavailable = fields.Boolean(
+        string='Address is unavailable'
+    )
+    ref_address_is_unavailable_selection = fields.Selection(
+        [('set', 'Set'),
+         ('remove', 'Remove'),
+         ], string='Address is unavailable:', default=False, readonly=False, required=False
+    )
+
+    ref_address_id = fields.Many2one(
+        comodel_name='clv.address',
+        string='Address'
+    )
+    ref_address_id_selection = fields.Selection(
+        [('set', 'Set'),
+         ('remove', 'Remove'),
+         ], string='Address:', default=False, readonly=False, required=False
+    )
+
+    get_ref_address_data = fields.Boolean(
+        string='Get Reference Address Data'
+    )
+
     verification_marker_ids = fields.Many2many(
         comodel_name='clv.verification.marker',
         relation='clv_family_mass_edit_verification_marker_rel',
@@ -39,6 +62,19 @@ class FamilyMassEdit(models.TransientModel):
         for family in self.family_ids:
 
             _logger.info(u'%s %s', '>>>>>', family.name)
+
+            if self.ref_address_is_unavailable_selection == 'set':
+                family.ref_address_is_unavailable = self.ref_address_is_unavailable
+            if self.ref_address_is_unavailable_selection == 'remove':
+                family.ref_address_is_unavailable = False
+
+            if self.ref_address_id_selection == 'set':
+                family.ref_address_id = self.ref_address_id.id
+            if self.ref_address_id_selection == 'remove':
+                family.ref_address_id = False
+
+            if self.get_ref_address_data:
+                family.do_family_get_ref_address_data()
 
             if self.verification_marker_ids_selection == 'add':
                 m2m_list = []
