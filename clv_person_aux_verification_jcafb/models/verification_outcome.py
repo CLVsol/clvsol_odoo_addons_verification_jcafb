3ab176f0f3d002c4fe077c8e5abcfae516bdb21a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-09-21 17:31:54 -0300

    [IMP] clv_person_aux_verification_jcafb (IMP models/verification_outcome.py)

--- clv_person_aux_verification_jcafb/models/verification_outcome.py
+++ clv_person_aux_verification_jcafb/models/verification_outcome.py
@@ -537,7 +537,7 @@ class VerificationOutcome(models.Model):
 
             if family.id is not False:
 
-                outcome_info = _('"Family" should not be set\n.')
+                outcome_info = _('"Family" should not be set.\n')
                 state = self._get_verification_outcome_state(state, 'Error (L1)')
 
             # outcome_info = _('"Family is Unavailable" should not be set.\n')
