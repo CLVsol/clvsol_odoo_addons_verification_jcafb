b737ddf9b4a2681325e11dc0b03d3bc11655f312
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-18 11:49:11 -0300

    [IMP] clv_family_verification_jcafb (IMP models/verification_outcome.py)

--- clv_family_verification_jcafb/models/verification_outcome.py
+++ clv_family_verification_jcafb/models/verification_outcome.py
@@ -121,7 +121,7 @@ class Family(models.Model):
                 if action_call:
 
                     verification_outcome.state = 'Unknown'
-                    verification_outcome.outcome_text = False
+                    verification_outcome.outcome_info = False
 
                     exec(action_call)
 
@@ -168,18 +168,18 @@ class VerificationOutcome(models.Model):
 
                 street_patern = PartnerEntityStreetPattern.search([
                     ('street', '=', model_object.street_name),
-                    ('district', '=', model_object.district),
+                    ('street2', '=', model_object.street2),
                 ])
 
                 if street_patern.street is False:
 
                     outcome_info += _('"Street Pattern" was not recognised.') + \
-                        ' (' + str(model_object.street_name) + ' [' + str(model_object.district) + '])\n'
+                        ' (' + str(model_object.street_name) + ' [' + str(model_object.street2) + '])\n'
                     state = self._get_verification_outcome_state(state, 'Warning (L0)')
 
                 if (model_object.zip is False) or \
                    (model_object.street_name is False) or \
-                   (model_object.district is False) or \
+                   (model_object.street2 is False) or \
                    (model_object.country_id is False) or \
                    (model_object.state_id is False) or \
                    (model_object.city_id is False):
@@ -190,8 +190,8 @@ class VerificationOutcome(models.Model):
                 # if (model_object.zip is False) or \
                 #    (model_object.street_name is False) or \
                 #    (model_object.street_number is False) or \
+                #    (model_object.street_number2 is False) or \
                 #    (model_object.street2 is False) or \
-                #    (model_object.district is False) or \
                 #    (model_object.country_id is False) or \
                 #    (model_object.state_id is False) or \
                 #    (model_object.city_id is False):
@@ -240,8 +240,8 @@ class VerificationOutcome(models.Model):
                 if (model_object.zip != ref_address.zip) or \
                    (model_object.street_name != ref_address.street_name) or \
                    (model_object.street_number != ref_address.street_number) or \
+                   (model_object.street_number2 != ref_address.street_number2) or \
                    (model_object.street2 != ref_address.street2) or \
-                   (model_object.district != ref_address.district) or \
                    (model_object.country_id != ref_address.country_id) or \
                    (model_object.state_id != ref_address.state_id) or \
                    (model_object.city_id != ref_address.city_id):
