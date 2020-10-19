bdaa3058637cc5e35bb70e9faacf83866bbdf855
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-19 17:37:20 -0300

    [REF] clv_person_verification_jcafb

--- clv_person_verification_jcafb/__manifest__.py
+++ clv_person_verification_jcafb/__manifest__.py
@@ -15,6 +15,7 @@
         'clv_verification',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'data/person_verification.xml',
         'views/verification_outcome_view.xml',
         'wizard/person_mass_edit_view.xml',
