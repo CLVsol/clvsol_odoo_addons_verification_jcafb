c3fc28e051df07e65fb75a5d5c19105abf383d5b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-11-15 13:09:42 -0300

    [ADD] clv_partner_entity_verification_jcafb

---
+++ clv_partner_entity_verification_jcafb/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Entity Verification (for CLVhealth-JCAFB Solution)',
+    'summary': 'Partner Entity Verification Module for CLVhealth-JCAFB Solution.',
+    'version': '14.0.4.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_partner_entity',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/partner_entity_street_pattern_view.xml',
+        'views/partner_entity_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
