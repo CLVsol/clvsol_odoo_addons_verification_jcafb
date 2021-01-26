07e7b4b32e9124403d07a7d9c162543cebcddbfb
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-01-26 13:41:29 -0300

    [IMP] clv_address_aux_verification_jcafb (IMP wizard/address_aux_reload.py)

--- clv_address_aux_verification_jcafb/wizard/address_aux_reload.py
+++ clv_address_aux_verification_jcafb/wizard/address_aux_reload.py
@@ -4,7 +4,7 @@
 
 import logging
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -13,10 +13,13 @@ class AddressAuxReload(models.TransientModel):
     _description = 'Address (Aux) Reload'
     _name = 'clv.address_aux.reload'
 
+    def _default_address_aux_ids(self):
+        return self._context.get('active_ids')
     address_aux_ids = fields.Many2many(
         comodel_name='clv.address_aux',
         relation='clv_address_aux_reload_rel',
-        string='Addresses (Aux)'
+        string='Addresses (Aux)',
+        default=_default_address_aux_ids
     )
 
     # related_address_verification_exec = fields.Boolean(
@@ -41,14 +44,14 @@ class AddressAuxReload(models.TransientModel):
         }
         return action
 
-    @api.model
-    def default_get(self, field_names):
+    # @api.model
+    # def default_get(self, field_names):
 
-        defaults = super().default_get(field_names)
+    #     defaults = super().default_get(field_names)
 
-        defaults['address_aux_ids'] = self.env.context['active_ids']
+    #     defaults['address_aux_ids'] = self.env.context['active_ids']
 
-        return defaults
+    #     return defaults
 
     def do_address_aux_reload(self):
         self.ensure_one()
